fe7387c147842f47fe6d2eef0f6e1dae2b552a7f
Shriyansh Raj Timsina <shriyanshraj.timsina@gmail.com>
2024-05-31 18:16:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "123abc";
+
+//console.log(typeof score);
+
+let valueInString = Number(score);
+
+//console.log(typeof valueInString);
+//console.log(valueInString);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "asasd" => true
+
+let someNumber = 34;
+
+let StringNumber = String(someNumber);
+console.log(typeof StringNumber);
