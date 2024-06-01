41b66c28b1db15449d67b05b07ca25076fd6c24b
Shriyansh Raj Timsina <shriyanshraj.timsina@gmail.com>
2024-06-01 18:25:57 +0000

    math and min

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const num = 12.24356;
+//console.log(num.toFixed(2));
+
+//console.log(num.toPrecision(5));
+
+const hundreds = 100000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++//
+
+// console.log(Math);
+
+// console.log(Math.abs(-3.2));
+// console.log(Math.round(2.5));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+// console.log(Math.min(2,3,7,1));
+// console.log(Math.max(11,21,10,78));
+
+//console.log(Math.random());
+
+//console.log(Math.floor(Math.random() * 6) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
