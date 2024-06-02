f0dd2fff5e75cb794f59028c0708268370575be3
Shriyansh Raj Timsina <shriyanshraj.timsina@gmail.com>
2024-06-02 07:35:57 +0000

    array part ine

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+
+//const myArr = [0, 1, 2, 3, 4, 5, true, "Shrii"];
+
+ const myArr = [1,2,3,4,5];
+ //console.log(myArr[1]);
+
+ const myHeros = ["ironman","spiderman","thor","thanos"];
+
+ const myArr2 = new Array(1,2,3,4,5);
+ //console.log(myArr2);
+ //console.log(myArr2.length);
+
+ // Array methods
+
+//push
+// myArr2.push(10);
+// myArr2.push(20);
+// //pop
+// myArr2.pop();
+
+//myArr2.unshift(99);
+//myArr2.unshift(100);
+//myArr2.shift();
+
+//console.log(myArr2.includes(9));
+// console.log(myArr2.indexOf(4));
+
+// const newArr = myArr2.join();
+
+// console.log(myArr2);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A" , myArr2);
+
+const myn1 = myArr2.slice(1,3);
+console.log(myn1);
+console.log("B", myArr2);
+
+const myn2 = myArr2.splice(1,3);
+console.log("C", myArr2);
+console.log(myn2);
