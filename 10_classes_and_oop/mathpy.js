403985cb6808501c5b2d7e63e1ea366991599bb2
Shriyansh Raj Timsina <shriyanshraj.timsina@gmail.com>
2024-06-14 09:46:47 +0000

    math object in js

---
+++ 10_classes_and_oop/mathpy.js
@@ -0,0 +1,32 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(descripter);
+// console.log(Math.PI);
+
+// const myNewObj = Object.create(null)
+
+const user = {
+    username: 'Shri',
+    score: 200,
+    isLoggedIn: true,
+
+    hello: function(){
+        console.log('hello');
+    }
+}
+
+// console.log(Object.getOwnPropertyDescriptor(user, "username"));
+
+// Object.defineProperty(user, 'username', {
+//     writable: false,
+//     enumerable: false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(user, "username"));
+
+
+for (const [key,value] of Object.entries(user)) {
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+    
+}
